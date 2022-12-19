8e0b6c0782afbd60dda9255f718faae8f9f226dc
Prince Gupta <princegupta1586@gmail.com>
2022-12-20 00:06:32 +0530

    added movies api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
 
 const app = express();
 dotenv.config();
@@ -25,6 +26,7 @@ app.use(express.json());
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("api/movie", movieRoute);
 
 app.listen(8800, () => {
   console.log("Backend Server is running!");
